fe8618d099868ac65a5753803dc78142f22259d5
unknown <dong.ze@21vianet.com>
2021-09-03 08:41:12 +0800

    项目初始化-调整初始化目录结构

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,16 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <!-- 路由入口 -->
     <router-view/>
+    <h1>黑马头条</h1>
   </div>
 </template>
 
+<script>
+export default {
+  // 根组件名称（建议每一个组件都给一个name）
+  name: 'App'
+}
+</script>
 <style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
---
+++ vue.config.js
@@ -0,0 +1,8 @@
+// vue脚手架项目 - 配置文件（类似webpack中的 webpack.config.js）
+
+module.exports = {
+  lintOnSave: false, // 关闭eslint语法检查
+  devServer: {
+    open: true // 启动项目时，自动弹出浏览器
+  }
+}
